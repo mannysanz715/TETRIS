4ff31bff12384f481b02fa94b15ea9039344c0fd
mannysanz715 <mannysanz715@gmail.com>
2022-12-19 11:51:27 -0700

    Fill Data file with array of objects for each block, name, position and color;

--- js/tetris-blocks.js
+++ js/tetris-blocks.js
@@ -1,15 +1,42 @@
 
 
-class Block {
-  constructor(name, startingPosition, color){
-    this.name = name
-    this.startingPosition = startingPosition
-    this.color = color
-  }
-}
-
-
-let square = new Block('square', [[0,1],[0,1]],'red')
+const blocks = [
+  {
+    name : 'oBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'zBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'iBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'lBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'jBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'sBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+  {
+    name : 'tBlock',
+    position : [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]],
+    color : 'red'
+  },
+]
 
 
 
